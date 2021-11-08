2763a7a1d50686a5c3fbaefe127fc6f4fe07613f
Moorehe2 <91582133+Moorehe2@users.noreply.github.com>
2021-11-08 01:42:03 -0500

    Add files via upload

---
+++ Immunizationtype.sql
@@ -0,0 +1,78 @@
+use immunizations;
+			
+
+
+Delimiter $$ 
+
+
+
+CREATE FUNCTION Immunizationtype(
+	DESCRIPTION DECIMAL(10,2)
+    )
+RETURNS VARCHAR(20)
+BEGIN
+	DECLARE Immunizationtype VARCHAr(20);
+    
+	IF DESCRIPTION >= 140 THEN
+
+    SET Immunizationtype = 'Flu vaccine';
+    
+ELSEIF (DESCRIPTION < 114 AND
+			DESCRIPTION = 113) THEN
+            
+	SET Immunizationtype = 'TD';
+
+ELSEIF DESCRIPTION < 50 then
+	SET Immunizationtype = 'Hep B';
+END IF;
+	-- RETURN THE Immunizaitontype
+    RETURN (Immunizationtype);
+END$$
+
+DELIMITER  ;
+
+
+Delimiter $$ 
+
+
+
+CREATE FUNCTION Immunizationtype(
+	DESCRIPTION DECIMAL(10,2)
+    )
+RETURNS VARCHAR(20)
+BEGIN
+	DECLARE Immunizationtype VARCHAr(20);
+    
+	IF DESCRIPTION >= 140 THEN
+
+    SET Immunizationtype = 'Flu vaccine';
+    
+ELSEIF (DESCRIPTION < 114 AND
+			DESCRIPTION = 113) THEN
+            
+	SET Immunizationtype = 'TD';
+
+ELSEIF DESCRIPTION < 50 then
+	SET Immunizationtype = 'Hep B';
+END IF;
+	-- RETURN THE Immunizaitontype
+    RETURN (Immunizationtype);
+END$$
+
+DELIMITER  ;
+
+
+Select 
+	PATIENT
+	DESCRIPTION
+
+
+    FROM
+    immunizations
+order by
+	DESCRIPTION, immunizationtype;
+	
+
+
+
+
